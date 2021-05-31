b070241e5bd799738b4e96809b680ee254e36450
Iprouteth0 <jmrandall05@gmail.com>
2021-05-31 13:32:46 -0500

    Fixed concatenation effect
    .

--- AddRom.sh
+++ AddRom.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 dialog --backtitle "Add ROM to ROM menu" --title "Rom - Form" \
 --form "\n'ROM make target' is most often bacon, but consult your ROM git readme.  For 'vendor config directory' use \$ROMNAME unless your rom uses a different vendor directory than the rom name.  This is most often seen with AOSP ROMs, but there are a few lineage based that do this too." 25 75 8 \
-"Name of ROM:" 1 1 "" 1 25 40 30  \
-"Menu number:" 2 1 "" 2 25 40 30  \
-"ROM git url:" 3 1 "" 3 25 40 30  \
+"Name of ROM:" 1 1 "" 1 25 65 120  \
+"Menu number:" 2 1 "" 2 25 10 30  \
+"ROM git url:" 3 1 "" 3 25 70 120  \
 "ROM make target:" 4 1 "" 4 25 40 30  \
-"Vendor config directory:" 5 1 "" 5 25 40 30 > /tmp/out.tmp \
+"Vendor config directory:" 5 1 "" 5 25 65 120 > /tmp/out.tmp \
 2>&1 >/dev/tty
 # Start retrieving each line from temp file 1 by one with sed and declare variables as inputs
 export CALLROM=`sed -n 1p /tmp/out.tmp`
