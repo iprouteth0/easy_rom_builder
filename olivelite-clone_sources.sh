3a854f8c77075176ea8a690e9cd704c44c6d6a47
Iprouteth0 <jmrandall05@gmail.com>
2021-05-31 13:40:07 -0500

    Typo
    .

--- olivelite-clone_sources.sh
+++ olivelite-clone_sources.sh
@@ -3,7 +3,7 @@
 ## Device Tree
 git clone https://github.com/Xiaomi-SDM439/android_device_xiaomi_olivelite device/xiaomi/olivelite
 git clone https://github.com/mi-msm8937/android_device_xiaomi_sdm439-common device/xiaomi/sdm439-common
-git cllne https://github.com/mi-msm8937/android_device_xiaomi_msm8937-common device/xiaomi/msm8937-common
+git clone https://github.com/mi-msm8937/android_device_xiaomi_msm8937-common device/xiaomi/msm8937-common
 ## Vendor Tree
 git clone https://github.com/mi-msm8937/proprietary_vendor_xiaomi_olives vendor/xiaomi/olives
 git clone https://github.com/mi-msm8937/proprietary_vendor_xiaomi_msm8937-common vendor/xiaomi/msm8937-common
