4c38da543e9538d3c4b724fb8fd89b41cf8057c9
Iprouteth0 <jmrandall05@gmail.com>
2021-06-19 09:05:48 -0500

    Updated trees
    Using updated trees

--- olivewood-clone_sources.sh
+++ olivewood-clone_sources.sh
@@ -1,21 +1,25 @@
 #! /bin/bash
 
 ## Device Tree
-git clone https://github.com/iprouteth0/device_xiaomi_olivewood device/xiaomi/olivewood
+#git clone https://github.com/iprouteth0/device_xiaomi_olivewood device/xiaomi/olivewood
+#git clone https://github.com/shield44/device_xiaomi_olivewood device/xiaomi/olivewood
+git clone https://github.com/DerpFest-Devices/device_xiaomi_olivewood device/xiaomi/olivewood
 
 ## Vendor Tree
-git clone https://github.com/iprouteth0/vendor_xiaomi_olives vendor/xiaomi/olives
+#git clone https://github.com/iprouteth0/vendor_xiaomi_olives vendor/xiaomi/olives
+git clone https://github.com/DerpFest-Devices/vendor_xiaomi_olivewood vendor/xiaomi/olives
 
 ## kernel sources
-git clone https://github.com/Xiaomi-SDM439-Development/android_kernel_xiaomi_sdm439 -b a11/clean-iwlan kernel/xiaomi/sdm439
+#git clone https://github.com/Xiaomi-SDM439-Development/android_kernel_xiaomi_sdm439 -b a11/clean-iwlan kernel/xiaomi/sdm439
+git clone https://github.com/mi-msm8937/android_kernel_xiaomi_sdm439 -b a11/clean-iwlankernel/xiaomi/sdm439
 
 ## match device tree files to rom tree
-cp device/xiaomi/olivewood/lineage_olivewood.mk device/xiaomi/olivewood/$ROMNAME\_olivewood.mk
-sed -i "s|vendor/lineage/config|vendor/$VENDOR_CONFIG/config|" device/xiaomi/olivewood/$ROMNAME\_olivewood.mk
-sed -i "s|lineage|$ROMNAME|" device/xiaomi/olivewood/$ROMNAME\_olivewood.mk
-sed -i "s|lineage|$ROMNAME|" device/xiaomi/olivewood/AndroidProducts.mk
+cp device/xiaomi/olivewood/derp_olivewood.mk device/xiaomi/olivewood/$ROMNAME\_olivewood.mk
+sed -i "s|vendor/derp/config|vendor/$VENDOR_CONFIG/config|" device/xiaomi/olivewood/$ROMNAME\_olivewood.mk
+sed -i "s|derp|$ROMNAME|" device/xiaomi/olivewood/$ROMNAME\_olivewood.mk
+sed -i "s|derp|$ROMNAME|" device/xiaomi/olivewood/AndroidProducts.mk
 #echo "WITH_GAPPS := true" >> device/xiaomi/olivewood/$ROMNAME\_olivewood.mk
-
+cat ../device_mk_changes-msm8937.txt >> device/xiaomi/olivewood/device.mk
 
 if [[ -d "vendor/$VENDOR_CONFIG/config" ]]
 then
