7cb93f9e68caadb0c680498df512e064eb8c94d6
Iprouteth0 <jmrandall05@gmail.com>
2021-07-09 16:27:35 -0500

    Initial draft
    Very early draft

---
+++ borneo-clone_sources.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+git clone https://github.com/MotorolaMobilityLLC/kernel-msm -b MMI-QZBS30.Q4-43-43-2 kernel/motorola/sm6115
+
+git clone https://github.com/MotorolaMobilityLLC/motorola-kernel-modules -b MMI-QZBS30.Q4-43-43-2 kernel/motorola/sm6115/modules
+
+git clone https://github.com/MotorolaMobilityLLC/vendor-qcom-opensource-wlan-qcacld-3.0 -b MMI-QZBS30.Q4-43-43-2 vendor/qcom/opensource
+
+git clone https://github.com/MotorolaMobilityLLC/vendor-qcom-opensource-wlan-qca-wifi-host-cmn -b MMI-QZBS30.Q4-43-43-2 vendor/qcom/opensource
+
+git clone https://github.com/MotorolaMobilityLLC/vendor-qcom-opensource-wlan-fw-api -b MMI-QZBS30.Q4-43-43-2 vendor/qcom/opensource 
+
+git clone https://github.com/MotorolaMobilityLLC/vendor-qcom-opensource-audio-kernel -b MMI-QZBS30.Q4-43-43-2 vendor/qcom/opensource
+
+git clone https://github.com/MotorolaMobilityLLC/kernel-msm-4.19-techpack-video -b MMI-QZBS30.Q4-43-43-2 kernel/motorola/sm6115/techpack/video
+
+git clone https://github.com/MotorolaMobilityLLC/kernel-msm-4.19-techpack-display -b MMI-QZBS30.Q4-43-43-2 kernel/motorola/sm6115/techpack/display
+
+git clone https://github.com/MotorolaMobilityLLC/kernel-msm-4.19-techpack-camera -b MMI-QZBS30.Q4-43-43-2 kernel/motorola/sm6115/techpack/camera
