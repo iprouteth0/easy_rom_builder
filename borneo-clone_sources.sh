aba762a98783ddf81599feff4bdd05dccc3fc2b5
Iprouteth0 <jmrandall05@gmail.com>
2021-07-09 17:13:01 -0500

    Paths
    Slightly more completed paths

--- borneo-clone_sources.sh
+++ borneo-clone_sources.sh
@@ -4,13 +4,13 @@ git clone https://github.com/MotorolaMobilityLLC/kernel-msm -b MMI-QZBS30.Q4-43-
 
 git clone https://github.com/MotorolaMobilityLLC/motorola-kernel-modules -b MMI-QZBS30.Q4-43-43-2 kernel/motorola/sm6115/modules
 
-git clone https://github.com/MotorolaMobilityLLC/vendor-qcom-opensource-wlan-qcacld-3.0 -b MMI-QZBS30.Q4-43-43-2 vendor/qcom/opensource
+git clone https://github.com/MotorolaMobilityLLC/vendor-qcom-opensource-wlan-qcacld-3.0 -b MMI-QZBS30.Q4-43-43-2 vendor/qcom/opensource/wlan/qcacld
 
-git clone https://github.com/MotorolaMobilityLLC/vendor-qcom-opensource-wlan-qca-wifi-host-cmn -b MMI-QZBS30.Q4-43-43-2 vendor/qcom/opensource
+git clone https://github.com/MotorolaMobilityLLC/vendor-qcom-opensource-wlan-qca-wifi-host-cmn -b MMI-QZBS30.Q4-43-43-2 vendor/qcom/opensource/wlan/qca-wifi-host-cmn
 
-git clone https://github.com/MotorolaMobilityLLC/vendor-qcom-opensource-wlan-fw-api -b MMI-QZBS30.Q4-43-43-2 vendor/qcom/opensource 
+git clone https://github.com/MotorolaMobilityLLC/vendor-qcom-opensource-wlan-fw-api -b MMI-QZBS30.Q4-43-43-2 vendor/qcom/opensource/wlan/fw-api
 
-git clone https://github.com/MotorolaMobilityLLC/vendor-qcom-opensource-audio-kernel -b MMI-QZBS30.Q4-43-43-2 vendor/qcom/opensource
+git clone https://github.com/MotorolaMobilityLLC/vendor-qcom-opensource-audio-kernel -b MMI-QZBS30.Q4-43-43-2 vendor/qcom/opensource/audio
 
 git clone https://github.com/MotorolaMobilityLLC/kernel-msm-4.19-techpack-video -b MMI-QZBS30.Q4-43-43-2 kernel/motorola/sm6115/techpack/video
 
