1e63a6114e54dd899d1069c05ba1aed500e85541
Iprouteth0 <jmrandall05@gmail.com>
2021-07-09 20:50:50 -0500

    .
    Slowly fixing

--- borneo-clone_sources.sh
+++ borneo-clone_sources.sh
@@ -1,19 +1,25 @@
 #! /bin/bash
 
-git clone https://github.com/MotorolaMobilityLLC/kernel-msm -b MMI-QZBS30.Q4-43-43-2 kernel/motorola/sm6115
+mkdir borneo-workspace
+cd borneo-workspace
 
-git clone https://github.com/MotorolaMobilityLLC/motorola-kernel-modules -b MMI-QZBS30.Q4-43-43-2 kernel/motorola/sm6115/modules
+git clone https://github.com/MotorolaMobilityLLC/kernel-msm -b MMI-QZBS30.Q4-43-43-2 kernel/
 
-git clone https://github.com/MotorolaMobilityLLC/vendor-qcom-opensource-wlan-qcacld-3.0 -b MMI-QZBS30.Q4-43-43-2 vendor/qcom/opensource/wlan/qcacld
+cd kernel/
 
-git clone https://github.com/MotorolaMobilityLLC/vendor-qcom-opensource-wlan-qca-wifi-host-cmn -b MMI-QZBS30.Q4-43-43-2 vendor/qcom/opensource/wlan/qca-wifi-host-cmn
+git subtree add -P techpack/audio https://github.com/MotorolaMobilityLLC/vendor-qcom-opensource-audio-kernel -b MMI-QZBS30.Q4-43-43-2
 
-git clone https://github.com/MotorolaMobilityLLC/vendor-qcom-opensource-wlan-fw-api -b MMI-QZBS30.Q4-43-43-2 vendor/qcom/opensource/wlan/fw-api
+git subtree add -P techpack/video https://github.com/MotorolaMobilityLLC/kernel-msm-4.19-techpack-video -b MMI-QZBS30.Q4-43-43-2
 
-git clone https://github.com/MotorolaMobilityLLC/vendor-qcom-opensource-audio-kernel -b MMI-QZBS30.Q4-43-43-2 vendor/qcom/opensource/audio
+git subtree add -P techpack/display https://github.com/MotorolaMobilityLLC/kernel-msm-4.19-techpack-display -b MMI-QZBS30.Q4-43-43-2
 
-git clone https://github.com/MotorolaMobilityLLC/kernel-msm-4.19-techpack-video -b MMI-QZBS30.Q4-43-43-2 kernel/motorola/sm6115/techpack/video
+git subtree add -P techpack/camera https://github.com/MotorolaMobilityLLC/kernel-msm-4.19-techpack-camera -b MMI-QZBS30.Q4-43-43-2
 
-git clone https://github.com/MotorolaMobilityLLC/kernel-msm-4.19-techpack-display -b MMI-QZBS30.Q4-43-43-2 kernel/motorola/sm6115/techpack/display
+git subtree add -P drivers/staging/qcacld-3.0 https://github.com/MotorolaMobilityLLC/vendor-qcom-opensource-wlan-qcacld-3.0 -b MMI-QZBS30.Q4-43-43-2 
 
-git clone https://github.com/MotorolaMobilityLLC/kernel-msm-4.19-techpack-camera -b MMI-QZBS30.Q4-43-43-2 kernel/motorola/sm6115/techpack/camera
+git subtree add -P drivers/staging/fw-api https://github.com/MotorolaMobilityLLC/vendor-qcom-opensource-wlan-fw-api -b MMI-QZBS30.Q4-43-43-2 
+
+git subtree add -P drivers/staging/qca-wifi-host-cmn https://github.com/MotorolaMobilityLLC/vendor-qcom-opensource-wlan-qca-wifi-host-cmn -b MMI-QZBS30.Q4-43-43-2 
+
+
+git clone https://github.com/MotorolaMobilityLLC/motorola-kernel-modules -b MMI-QZBS30.Q4-43-43-2
