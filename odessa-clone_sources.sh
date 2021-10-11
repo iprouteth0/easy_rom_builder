54111c9a83759a18e0a59bcdb0b57366af3a443f
Iprouteth0 <jmrandall05@gmail.com>
2021-10-11 16:41:09 -0500

    Initial edit
    Odessa

---
+++ odessa-clone_sources.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+
+git clone https://github.com/iprouteth0/android_device_motorola_odessa device/motorola/odessa
+
+git clone https://github.com/iprouteth0/android_device_motorola_sm6150-common device/motorola/sm6150-common
+
+git clone https://github.com/iprouteth0/vendor_motorola_odessa vendor/motorola/odessa
+
+git clone https://github.com/iprouteth0/vendor_motorola_sm6150-common vendor/motorola/sm6150-common
+
+git clone https://github.com/iprouteth0/kernel_motorola_hanoip kernel/motorola/
+
+
+
+sed -i "s|vendor/lineage/config|vendor/$VENDOR_CONFIG/config|" device/motorola/odessa/lineage_odessa.mk
+sed -i "s|lineage_odessa|$ROMNAME\_odessa|" device/motorola/odessa/lineage_odessa.mk
+sed -i "s|lineage_|$ROMNAME\_|" device/motorola/odessa/AndroidProducts.mk
+cp device/motorola/odessa/lineage.dependencies device/motorola/odessa/$ROMNAME.dependencies
+cp device/motorola/odessa/lineage_ginna.mk device/motorola/odessa/$ROMNAME\_odessa.mk
+
+
+
+if [[ -d "vendor/$VENDOR_CONFIG/config" ]]
+then
+if [[ ! -f "vendor/$VENDOR_CONFIG/config/common_full_phone.mk" ]]
+then
+    sed -i "s|common_full_phone.mk|common.mk|" device/motorola/odessa/$ROMNAME\_odessa.mk
+fi
+fi
+
+if [[ -d "vendor/$VENDOR_CONFIG/configs" ]]
+then
+if [[ ! -f "vendor/$VENDOR_CONFIG/configs/common_full_phone.mk" ]]
+then
+    sed -i "s|common_full_phone.mk|common.mk|" device/motorola/odessa/$ROMNAME\_odessa.mk
+fi
+fi
